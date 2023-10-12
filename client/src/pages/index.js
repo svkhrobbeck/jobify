7291575ff15df3ef027025cc4f65e8c58a2b144b
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-12 21:52:00 +0500

    Create EditJob page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,7 @@
 export { default as AddJob } from "./AddJob";
 export { default as Admin } from "./Admin";
 export { default as AllJobs } from "./AllJobs";
+export { default as EditJob } from "./EditJob";
 export { default as Error } from "./Error";
 export { default as Landing } from "./Landing";
 export { default as Login } from "./Login";
