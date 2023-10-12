90be5f7e2c4ddd8d423229ddb2c34d2a994fbe8e
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-12 21:41:40 +0500

    Create DashboardLayout with context

---
+++ client/src/layouts/DashboardLayout.jsx
@@ -0,0 +1,39 @@
+import { Outlet } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Dashboard";
+import { createContext, useContext, useState } from "react";
+
+const DashboardContext = createContext();
+
+const DashboardLayout = ({ isDarkThemeEnabled }) => {
+  const user = { name: "Suhrobbek" };
+  const [sidebar, setSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeEnabled);
+
+  const toggleTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(prev => !prev);
+    document.body.classList.toggle("dark-theme", newDarkTheme);
+    localStorage.setItem("dark-theme", newDarkTheme);
+  };
+
+  const toggleSidebar = () => setSidebar(prev => !prev);
+
+  const logoutUser = async () => console.log("logout");
+
+  return (
+    <DashboardContext.Provider value={{ user, sidebar, isDarkTheme, toggleSidebar, toggleTheme, logoutUser }}>
+      <Wrapper>
+        <main className="dashboard">
+          <div>
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </DashboardContext.Provider>
+  );
+};
+
+export const useDashboardContext = () => useContext(DashboardContext);
+export default DashboardLayout;
