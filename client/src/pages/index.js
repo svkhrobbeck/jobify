e257a8d70c45e87c756b76d0e07226da0d59376b
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-12 21:50:54 +0500

    Create AddJob page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,4 @@
+export { default as AddJob } from "./AddJob";
 export { default as Error } from "./Error";
 export { default as Landing } from "./Landing";
 export { default as Login } from "./Login";
