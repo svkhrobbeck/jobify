19b7da0d4721ae119f74ba4aa76d8b7b2317363d
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-12 21:49:28 +0500

    Add context features

--- client/src/components/ThemeToggle.jsx
+++ client/src/components/ThemeToggle.jsx
@@ -1,7 +1,9 @@
 import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 import Wrapper from "../assets/wrappers/ThemeToggle";
+import { useDashboardContext } from "../layouts/DashboardLayout";
 
 const ThemeToggle = () => {
+  const { isDarkTheme, toggleTheme } = useDashboardContext();
 
   return (
     <Wrapper onClick={toggleTheme}>
