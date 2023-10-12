49da03de64223af02d6bd10cfa1817bca173d137
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-12 21:43:59 +0500

    Create Navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import Wrapper from "../assets/wrappers/Navbar";
+import { FaAlignLeft } from "react-icons/fa";
+import { Logo, LogoutContainer, ThemeToggle } from ".";
+import { useDashboardContext } from "../layouts/DashboardLayout";
+
+const Navbar = () => {
+  const { toggleSidebar } = useDashboardContext();
+
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <button className="toggle-btn" type="button" onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className="logo-text">dashboard</h4>
+        </div>
+        <div className="btn-container">
+          <ThemeToggle />
+          <LogoutContainer />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+export default Navbar;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,5 @@ export { default as FormRow } from "./FormRow";
 export { default as Logo } from "./Logo";
 export { default as LogoutContainer } from "./LogoutContainer";
 export { default as NavLinks } from "./NavLinks";
+export { default as Navbar } from "./Navbar";
 export { default as ThemeToggle } from "./ThemeToggle";
