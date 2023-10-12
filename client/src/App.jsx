9b495be5e3db58227dc1dd5915b9609231ac12ea
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-12 21:48:45 +0500

    Add layouts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,17 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { DashboardLayout, HomeLayout } from "./layouts";
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "dashboard",
+        element: <DashboardLayout isDarkThemeEnabled={isDarkThemeEnabled} />,
+      },
+    ],
+  },
 ]);
 
 const App = () => {
