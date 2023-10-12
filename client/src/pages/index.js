da1193fb3b9a01c536eaaca98b34b408834d686a
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-12 21:51:41 +0500

    Create AllJobs page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,6 @@
 export { default as AddJob } from "./AddJob";
 export { default as Admin } from "./Admin";
+export { default as AllJobs } from "./AllJobs";
 export { default as Error } from "./Error";
 export { default as Landing } from "./Landing";
 export { default as Login } from "./Login";
