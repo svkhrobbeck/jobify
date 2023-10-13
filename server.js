72215eec881296cae651667b59ca6fb541d5886f
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-13 10:51:18 +0500

    Create JobRouter and add server.js JobRouter

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+// routers
+import jobRouter from "./routers/jobRouter.js";
 
 const app = express();
 const { PORT, NODE_ENV } = process.env;
@@ -10,6 +12,10 @@ app.use(express.json());
 if (NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// endpoints
+app.use("/api/v1/jobs", jobRouter);
+
 
 // listen handlers
 connectDB();
