980d6d1c7cc38f3358ba6ef67209bca9c75bb9d1
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-13 15:38:30 +0500

    Add all controllers to jobRouter

--- routers/jobRouter.js
+++ routers/jobRouter.js
@@ -1,3 +1,8 @@
 import { Router } from "express";
+import { createJob, deleteJob, editJob, getAllJobs, getSingleJob } from "../controllers/jobController.js";
+const router = Router();
+
+router.route("/").get(getAllJobs).post(createJob);
+router.route("/:id").get(getSingleJob).patch(editJob).delete(deleteJob);
 
 export default router;
