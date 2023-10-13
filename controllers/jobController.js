d928ab4c412e42ece493713c761dd7e3ed3831d1
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-13 15:41:24 +0500

    Remove unnecessary initial validations

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,49 +1,42 @@
+import { StatusCodes } from "http-status-codes";
 import Job from "../models/jobModel.js";
-import { isValidObjectId } from "mongoose";
+import { NotFoundError } from "../errors/customError.js";
 
 // GET ALL JOBS
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find();
-  res.status(200).json({ jobs });
+  res.status(StatusCodes.OK).json({ jobs });
 };
 
 // CREATE A JOB
 export const createJob = async (req, res) => {
-  const { position, company, jobLocation } = req.body;
-
-  if (!position.trim() || !company.trim()) {
-    return res.status(400).json({ msg: "Company and position are required" });
-  }
-
-  if (jobLocation !== undefined && jobLocation.length < 5) {
-    return res.status(400).json({ msg: "Location must be at least 5 characters long" });
-  }
-
   const job = await Job.create(req.body);
-  res.status(201).json({ job });
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 // GET SINGLE JOB
 export const getSingleJob = async (req, res) => {
   const { id } = req.params;
-
-  if (!isValidObjectId(id)) {
-    return res.status(404).json({ msg: `invalid id` });
-  }
-
   const job = await Job.findById(id);
 
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id: ${id}` });
-  }
-
+  if (!job) throw new NotFoundError(`no job with id: ${id}`);
   res.status(200).json({ job });
 };
 
 // EDIT JOB
 export const editJob = async (req, res) => {
+  const { id } = req.params;
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, { new: true });
+
+  if (!updatedJob) throw new NotFoundError(`no job with id: ${id}`);
+  res.status(StatusCodes.OK).json({ job: updatedJob });
 };
 
 // DELETE JOB
 export const deleteJob = async (req, res) => {
+  const { id } = req.params;
+  const removedJob = await Job.findByIdAndDelete(id);
+
+  if (!removedJob) throw new NotFoundError(`no job with id: ${id}`);
+  res.status(StatusCodes.OK).json({ msg: "deleted", job: removedJob });
 };
