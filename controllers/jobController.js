f5becf7c4cc35669c88827ee766bfaac6295114b
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-13 11:48:34 +0500

    Create jobController

---
+++ controllers/jobController.js
@@ -0,0 +1,21 @@
+import Job from "../models/jobModel.js";
+
+// GET ALL JOBS
+export const getAllJobs = async (req, res) => {
+};
+
+// CREATE A JOB
+export const createJob = async (req, res) => {
+};
+
+// GET SINGLE JOB
+export const getSingleJob = async (req, res) => {
+};
+
+// EDIT JOB
+export const editJob = async (req, res) => {
+};
+
+// DELETE JOB
+export const deleteJob = async (req, res) => {
+};
