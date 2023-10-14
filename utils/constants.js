d82283fb70caba5d9471ffed45af53d2ae07745a
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-14 22:45:10 +0500

    Create constants

---
+++ utils/constants.js
@@ -0,0 +1,19 @@
+export const JOB_STATUS = {
+  PENDING: "pending",
+  INTERVIEW: "interview",
+  DECLINED: "declined",
+};
+
+export const JOB_TYPE = {
+  FULL_TIME: "full-time",
+  PART_TIME: "part-time",
+  INTERNSHIP: "internship",
+  REMOTE: "remote",
+};
+
+export const JOB_SORT_TYPE = {
+  NEWEST_FIRST: "newest",
+  NEWEST_FIRST: "oldest",
+  ASCENDING: "a-z",
+  DESCENDING: "z-a",
+};
