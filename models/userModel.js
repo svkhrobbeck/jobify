0772bc504c9a06d712952bc9c8f4aac137f220c9
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-15 15:50:08 +0500

    Add userSchema to userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,25 @@
 import { Schema, model } from "mongoose";
+import { USER_ROLE } from "../utils/constants.js";
 
 const reqString = { type: String, required: true };
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  name: reqString,
+  email: reqString,
+  password: reqString,
+  lastName: {
+    type: String,
+    default: "lastName",
+  },
+  location: {
+    type: String,
+    default: "Tashkent",
+  },
+  role: {
+    type: String,
+    enum: USER_ROLE,
+    default: USER_ROLE.USER,
+  },
+});
 
 export default model("User", userSchema);
