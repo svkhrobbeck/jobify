59056c02670f55f7f7cf3513d53008f665bea5f2
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-17 19:07:51 +0500

    add createdBy to createJob controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -9,6 +9,7 @@ export const getAllJobs = async (req, res) => {
 
 // CREATE A JOB
 export const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
