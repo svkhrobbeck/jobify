2cd2efb8a6ebe29c8cae21683bbd96beb34e4780
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-17 11:58:13 +0500

    add set user or admin logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,8 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
 
 export const register = async (req, res) => {
+  const isFirstUser = (await User.countDocuments()) === 0;
+  req.body.role = isFirstUser ? "admin" : "user";
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
