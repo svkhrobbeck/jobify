6de59ed6fab65d4d09d6d3d63df7d7e3f1c5fb5e
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-17 19:06:31 +0500

    add cookieParser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import "express-async-errors";
 import express from "express";
 import "dotenv/config";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 // routers
 import jobRouter from "./routers/jobRouter.js";
@@ -12,7 +13,9 @@ import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.js";
 const app = express();
 const { PORT, NODE_ENV } = process.env;
 
+app.use(cookieParser());
 app.use(express.json());
+
 if (NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
