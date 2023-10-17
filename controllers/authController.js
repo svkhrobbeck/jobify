b01ba7a107e45751e893b2aa484090bd01b798f0
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-17 19:09:12 +0500

    add hash password to register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,18 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customError.js";
+import { createJwt } from "../utils/tokenUtils.js";
 
 export const register = async (req, res) => {
   const isFirstUser = (await User.countDocuments()) === 0;
   req.body.role = isFirstUser ? "admin" : "user";
+
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
 export const login = async (req, res) => {
