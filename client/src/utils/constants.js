10da9b0386eabf7c32d195f9abfbcccea32bc3ec
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-20 12:43:47 +0500

    Create constants

---
+++ client/src/utils/constants.js
@@ -0,0 +1,24 @@
+export const JOB_STATUS = {
+  PENDING: "pending",
+  INTERVIEW: "interview",
+  DECLINED: "declined",
+};
+
+export const JOB_TYPE = {
+  FULL_TIME: "full-time",
+  PART_TIME: "part-time",
+  INTERNSHIP: "internship",
+  REMOTE: "remote",
+};
+
+export const JOB_SORT_TYPE = {
+  NEWEST_FIRST: "newest",
+  NEWEST_FIRST: "oldest",
+  ASCENDING: "a-z",
+  DESCENDING: "z-a",
+};
+
+export const USER_ROLE = {
+  USER: "user",
+  ADMIN: "admin",
+};
