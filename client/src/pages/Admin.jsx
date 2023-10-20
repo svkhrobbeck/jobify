00a0f3499575ced1d8957d74deb86a5b38ab675a
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-20 17:52:04 +0500

    Create adminLoader

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,4 +1,19 @@
+import { FaSuitcaseRolling, FaCalendarCheck } from "react-icons/fa";
+import { toast } from "react-toastify";
+import { useLoaderData, redirect } from "react-router-dom";
+import Wrapper from "../assets/wrappers/StatsContainer";
+import customAxios from "../utils/customAxios";
+
+export const adminLoader = async () => {
+  try {
+    const { data } = await customAxios.get("/users/admin/statistics");
+    return data;
+  } catch (err) {
+    toast.error(err?.response?.data?.msg);
+    return redirect("/dashboard");
+  }
+};
+
 const Admin = () => {
-  return <div>Admin</div>;
 };
 export default Admin;
