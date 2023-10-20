91dd2f4b13eec04fef0220842bceee178653535f
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-20 12:43:35 +0500

    Create FormRowSelect component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 export { default as BigSidebar } from "./BigSidebar";
 export { default as FormRow } from "./FormRow";
+export { default as FormRowSelect } from "./FormRowSelect.jsx";
 export { default as Logo } from "./Logo";
 export { default as LogoutContainer } from "./LogoutContainer";
 export { default as NavLinks } from "./NavLinks";
