1f556a33ff08c6a81ceb5a93d451138fe5837127
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-20 16:38:26 +0500

    Complete editJobLoader

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,9 +1,19 @@
 import { toast } from "react-toastify";
 import checkToastThemeOption from "../utils/checkToastThemeOption";
 import customAxios from "../utils/customAxios";
-import { useLoaderData } from "react-router-dom";
+import { Form, useNavigation, redirect, useLoaderData } from "react-router-dom";
+import { FormRow, FormRowSelect } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
 
 export const editJobLoader = async ({ params }) => {
+  try {
+    const { data } = await customAxios.get(`/jobs/${params.id}`);
+    return data;
+  } catch (err) {
+    toast.error(err?.response?.data?.msg, checkToastThemeOption());
+    return err;
+  }
 };
 
 export const editJobAction = async ({ request }) => {
