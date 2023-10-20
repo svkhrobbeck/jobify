3336e59ec31e168797f986946ccc89873f6379ca
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-20 15:49:07 +0500

    Remove login create cookie logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,18 +21,5 @@ export const login = async (req, res) => {
   const isValidUser = user && (await comparePassword(req.body.password, user.password));
   if (!isValidUser) throw new UnauthenticatedError("invalid credentials");
 
-  const oneDay = 1000 * 60 * 60 * 24;
-
-  const token = createJwt({ userId: user._id, role: user.role });
-  res.cookie("token", token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
-    secure: process.env.NODE_ENV === "production",
-  });
-  res.status(StatusCodes.OK).json({ msg: "user logged in" });
-};
-
-export const logout = (req, res) => {
-  res.cookie("token", "logout", { httpOnly: true, expire: new Date(Date.now()) });
-  res.status(StatusCodes.OK).json({ msg: "user logged out" });
+  const access_token = createJwt({ userId: user._id, role: user.role });
 };
