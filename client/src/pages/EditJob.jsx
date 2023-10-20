fcf0be1734b8ede8f26f883708aa7d3d15e98e87
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-20 15:54:12 +0500

    create EditJobLoader

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,3 +1,11 @@
+import { toast } from "react-toastify";
+import checkToastThemeOption from "../utils/checkToastThemeOption";
+import customAxios from "../utils/customAxios";
+import { useLoaderData } from "react-router-dom";
+
+export const editJobLoader = async ({ params }) => {
+};
+
 const EditJob = () => {
   return <div>EditJob</div>;
 };
