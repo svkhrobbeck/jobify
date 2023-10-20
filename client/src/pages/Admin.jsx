9cfd6918c600d12193f0762fc7dbba4f9f0f4182
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-20 18:22:44 +0500

    Update admin page

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -3,17 +3,33 @@ import { toast } from "react-toastify";
 import { useLoaderData, redirect } from "react-router-dom";
 import Wrapper from "../assets/wrappers/StatsContainer";
 import customAxios from "../utils/customAxios";
+import checkToastThemeOption from "../utils/checkToastThemeOption";
+import { StatItem } from "../components";
 
 export const adminLoader = async () => {
   try {
     const { data } = await customAxios.get("/users/admin/statistics");
     return data;
   } catch (err) {
-    toast.error(err?.response?.data?.msg);
+    toast.error(err?.response?.data?.msg, checkToastThemeOption());
     return redirect("/dashboard");
   }
 };
 
 const Admin = () => {
+  const { totalJobs, totalUsers } = useLoaderData();
+
+  return (
+    <Wrapper>
+      <StatItem
+        count={totalUsers}
+        title="current users"
+        color="#e9b949"
+        bgColor="#fcefc7"
+        icon={<FaSuitcaseRolling />}
+      />
+      <StatItem count={totalJobs} title="total jobs" color="#647acb" bgColor="#e0e8f9" icon={<FaCalendarCheck />} />
+    </Wrapper>
+  );
 };
 export default Admin;
