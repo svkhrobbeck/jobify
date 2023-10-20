fd5b5f6738ba7213fcfd5c413d01497f04c61e91
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-20 15:45:56 +0500

    Configure axios interceptors

--- client/src/utils/customAxios.js
+++ client/src/utils/customAxios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
 const customAxios = axios.create({
-  baseURL: "/api/v1",
+
+customAxios.interceptors.request.use(config => {
+  const token = localStorage.getItem("t$o@k!en*");
+  const authorization = token ? `Token ${token}` : "";
+  config.headers.Authorization = authorization;
+
+  return config;
 });
 
 export default customAxios;
