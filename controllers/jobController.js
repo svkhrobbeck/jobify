b825484771a6bd7b6b9d6c57b2011eb60f2cb31e
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-21 15:48:48 +0500

    get stats and monthly applications from mongodb

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,4 +1,6 @@
 import { StatusCodes } from "http-status-codes";
+import { Types } from "mongoose";
+import day from "dayjs";
 import Job from "../models/jobModel.js";
 
 // GET ALL JOBS
@@ -34,4 +36,48 @@ export const deleteJob = async (req, res) => {
 
 // SHOW STATISTICS
 export const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
+    { $match: { createdBy: new Types.ObjectId(req.user.userId) } },
+    { $group: { _id: "$jobStatus", count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, curr) => {
+    acc[curr._id] = curr.count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: new Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } },
+    { $limit: 6 },
+  ]);
+
+  monthlyApplications = monthlyApplications
+    .map(item => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+
+      const date = day()
+        .month(month - 1)
+        .year(year)
+        .format("MMM YY");
+      return { date, count };
+    })
+    .reverse();
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
