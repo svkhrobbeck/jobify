18a7d3ea7f8e9d30a3bdb7f9caf109602375bd97
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-21 16:58:17 +0500

    Update axios baseUrl

--- client/src/utils/customAxios.js
+++ client/src/utils/customAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customAxios = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_APP_BASE_API_URL,
 });
 
 customAxios.interceptors.request.use(config => {
