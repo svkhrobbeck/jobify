5963da6fbf2a770e172e677fb27dfcad0d8471f0
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-22 10:13:26 +0500

    update db.js config

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 const connectDB = async () => {
   try {
-    const state = await connect(process.env.MONGO_URI, { useNewUrlParser: true });
+    const state = await connect(process.env.MONGO_URI);
     console.log(`MongoDB connected to ${state.connection.host}`);
   } catch (err) {
     console.log(err);
