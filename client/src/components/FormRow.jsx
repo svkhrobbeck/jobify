fd692db4c8d93f2bf70183ae2041306510af9a3a
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-23 21:30:50 +0500

    refactor: add refactor  prop

--- client/src/components/FormRow.jsx
+++ client/src/components/FormRow.jsx
@@ -1,10 +1,24 @@
-const FormRow = ({ type = "text", name, labelText, defaultValue = "" }) => {
+const FormRow = ({
+  type = "text",
+  name,
+  labelText,
+  defaultValue = "",
+  onChange = e => {},
+}) => {
   return (
     <div className="form-row">
       <label className="form-label" htmlFor={name}>
         {labelText || name}
       </label>
-      <input className="form-input" type={type} id={name} name={name} defaultValue={defaultValue} required />
+      <input
+        className="form-input"
+        type={type}
+        id={name}
+        name={name}
+        defaultValue={defaultValue}
+        required
+        onChange={e => onChange(e)}
+      />
     </div>
   );
 };
