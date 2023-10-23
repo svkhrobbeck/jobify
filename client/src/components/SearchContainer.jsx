b0cf14aa94562eb2d485becdf2b3da00c6cd17d3
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-23 21:31:30 +0500

    refactor: add default values, change events and debounce

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,39 +1,59 @@
-import { FormRow, FormRowSelect, SubmitBtn } from ".";
+import { FormRow, FormRowSelect } from ".";
 import Wrapper from "../assets/wrappers/DashboardFormPage";
 import { Form, useSubmit, Link } from "react-router-dom";
 import { JOB_TYPE, JOB_STATUS, JOB_SORT_TYPE } from "../utils/constants";
 import { useAllJobsContext } from "../pages/AllJobs";
+import { useRef } from "react";
 
 const SearchContainer = () => {
-  const { data } = useAllJobsContext();
+  const { searchValues } = useAllJobsContext();
+  const { search, jobStatus, jobType, sort } = searchValues;
+  const submit = useSubmit();
+  const timeoutId = useRef(0);
+
+  const handleSearchJobs = e => {
+    clearTimeout(timeoutId.current);
+    const form = e.currentTarget.form;
+
+    timeoutId.current = setTimeout(() => {
+      submit(form);
+    }, 800);
+  };
 
   return (
     <Wrapper>
       <Form>
         <h5 className="form-title">search form</h5>
         <div className="form-center">
-          <FormRow type="search" name="search" />
+          <FormRow
+            type="search"
+            name="search"
+            defaultValue={search}
+            onChange={handleSearchJobs}
+          />
           <FormRowSelect
             labelText="job status"
             name="jobStatus"
             list={["all", ...Object.values(JOB_STATUS)]}
-            defaultValue="all"
+            defaultValue={jobStatus}
+            onChange={e => submit(e.currentTarget.form)}
           />
           <FormRowSelect
             labelText="job type"
             name="jobType"
             list={["all", ...Object.values(JOB_TYPE)]}
-            defaultValue="all"
+            defaultValue={jobType}
+            onChange={e => submit(e.currentTarget.form)}
           />
           <FormRowSelect
             name="sort"
             list={Object.values(JOB_SORT_TYPE)}
-            defaultValue="newest"
+            defaultValue={sort}
+            onChange={e => submit(e.currentTarget.form)}
           />
           <Link className="btn form-btn delete-btn" to="/dashboard/all-jobs">
             Reset Search Values
           </Link>
-          <SubmitBtn formBtn />
         </div>
       </Form>
     </Wrapper>
